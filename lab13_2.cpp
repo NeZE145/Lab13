4a2a5361b2245af55618ad2fc4c7f54a18c284b1
NeZE <boss-NeZE@hotmail.com>
2021-12-22 20:58:45 +0700

    lab13

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int gcd(int,int);
+
+
 int main(){
     int x, y;
-    cin >> x >> y;
+    cout << "x";
+    cin >> x ;
+    cout << "y";
+    cin >> y ;
     cout << gcd(x,y);
     return 0; 
+}
+
+
+int gcd(int x, int y) {
+   if (y == 0)
+   return x;
+   return gcd(y, x %y);
 }
